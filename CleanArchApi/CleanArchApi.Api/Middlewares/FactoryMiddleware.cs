1d4ac7741f80623023b593c2aef9dd9ad7dbfcfa
Abdul Hameed <hameed@tuli.com>
2025-03-16 00:45:06 +0500

    Add middlewares

---
+++ CleanArchApi/CleanArchApi.Api/Middlewares/FactoryMiddleware.cs
@@ -0,0 +1,11 @@
+namespace CleanArchApi.CleanArchApi.Api.Middlewares;
+
+public class FactoryMiddleware(ILogger<FactoryMiddleware> logger): IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        logger.LogInformation("Before the request (FactoryMiddleware)");
+        await  next(context);
+        logger.LogInformation("After the request (FactoryMiddleware)");
+    }
+}
